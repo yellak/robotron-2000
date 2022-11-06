115f3637273db2f5cf62f8c6f13f957d4febbfcf
Kálley Wilkerson <kalleywra@gmail.com>
2022-11-06 12:12:37 -0300

    Making the first two exercixes

---
+++ ex2/script.js
@@ -0,0 +1,7 @@
+const button_calcular = document.getElementById('calcular')
+
+button_calcular.addEventListener('click', onCalcularClick)
+
+function onCalcularClick() {
+    alert('Fui Clicado')
+}
