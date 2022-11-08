28f178432866c729d755c0f8f31f1f0c96857e75
Kálley Wilkerson <kalleywra@gmail.com>
2022-11-08 12:02:47 -0300

    Adding and subtracting in each field

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-const down = document.querySelector('#down')
-const up = document.querySelector('#up')
-const arms = document.querySelector('#arms')
+function manipulateValue(parent, action) {
+    piece = parent.querySelector(".controle-contador")
+    if (action === "+") {
+        piece.value = parseInt(piece.value) + 1
+        return
+    }
+    if (action === "-") {
+        piece.value = parseInt(piece.value) - 1
+        return
+    }
+}
 
-up.addEventListener('click', (event) => {
-    arms.value = parseInt(arms.value) + 1
-})
-
-down.addEventListener('click', (event) => {
-    arms.value = parseInt(arms.value) - 1
-})
+const control = document.querySelectorAll(".controle-ajuste")
+control.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        manipulateValue(event.target.parentNode, event.target.textContent)
+    })
+});
