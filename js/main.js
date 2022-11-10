2ce7bf95c785b7d65f73413b528cb42fd1a4c5e5
Kálley Wilkerson <kalleywra@gmail.com>
2022-11-09 23:59:18 -0300

    Updating robot stats after changing equipments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const control = document.querySelectorAll("[data-operation]")
+const stats = document.querySelectorAll("[data-stats]")
+
+
 control.forEach((element) => {
     element.addEventListener("click", (event) => {
-        manipulateValue(event.target.parentNode, event.target.dataset.operation)
+        target = event.target
+        manipulateValue(target.parentNode, target.dataset.operation)
+        updateStats(target.dataset.piece)
     })
 });
 
@@ -14,5 +19,45 @@ function manipulateValue(parent, action) {
     if (action === "-") {
         piece.value = parseInt(piece.value) - 1
         return
+    }
+}
+
+function updateStats(piece) {
+    stats.forEach((stat) => {
+        stat.textContent = parseInt(stat.textContent) + pecas[piece][stat.dataset.stats]
+    })
+}
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
